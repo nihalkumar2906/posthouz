788fe83d6d606a836886caf81429e72e845bf594
Nihal Kumar <nihalkumar2906@gmail.com>
2026-08-04 19:02:53 +0530

    Update site from Framer changes (new JS bundles, updated content)

---
+++ js/searchIndex-cVaOEaG184HD.js
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Error><Code>AccessDenied</Code><Message>Access Denied</Message><RequestId>MPCZBQ2Q210A0SQC</RequestId><HostId>qJTEe1INxlUBGXTPGmFaua/P3/E9daviYU6shm2K/5wrTQBTMbSa+dliOPTiKke/U6i5VD/6dJRqdV7YRH14vVAIgut4NNv2</HostId></Error>
